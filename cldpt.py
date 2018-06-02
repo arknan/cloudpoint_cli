fd6d6aa2b17636bfdd4854d8f9a816dd06bfd0c3
Ashwin Ramakrishnan <ashwinramki@gmail.com>
2018-06-01 22:45:14 -0400

    Added code to print help when no arguments were provided

--- cldpt.py
+++ cldpt.py
@@ -3,24 +3,32 @@
 
 import argparse
 import api
+import sys
 
 def main() :
     get_choices = sorted(["agents", "plugins", "assets", "schedules", "policies",\
         "replication", "granules", "classification-tags", "tasks", "jointokens",\
         "telemetry", "roles", "version", "licenses",\
         "reports", "report-types" ])
+
     parser = argparse.ArgumentParser(description='CloudPoint CLI help')
 
 #By default action="store" and type="string"
     parser.add_argument("-g", "--get",help="Get information on some attribute.\
     Allowed values are: {" +", ".join(get_choices) +"}", choices=get_choices, metavar='<option>')
+
     parser.add_argument("-a", "--authenticate", action="store_true",\
         help="Login to CloudPoint ; Required for doing any other operation")
+
     parser.add_argument("-c", "--create", metavar="<option>",\
     help="Create any information within CloudPoint")
 
     args = parser.parse_args()
-    interface(args)
+    if len(sys.argv) == 1 : 
+        parser.print_help()
+        sys.exit(1)
+    else :
+        interface(args)
 
 def interface(args) :
     x = api.Command()
