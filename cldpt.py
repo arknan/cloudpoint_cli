c883a1d920dfe13c2a6ee7a10cc2d69929e766e5
Ashwin Ramakrishnan <ashwinramki@gmail.com>
2018-06-01 23:54:37 -0400

    Modified code to move away from mindless if/elif/else and instead use a dictionary based switch

--- cldpt.py
+++ cldpt.py
@@ -5,17 +5,35 @@ import argparse
 import api
 import sys
 
+get_arg_mappings = {
+    "ad-config" : "idm/config/ad",
+    "agents" : "agents/",
+    "assets" : "assets/",
+    "classification-tags" : "classifications/tags",
+    "granules" : "granules",
+    "jointokens" : "jointokens/",
+    "licenses" : "licenses/",
+    "plugins" : "plugins/",
+    "policies" : "policies/",
+    "replication" : "replication/",
+    "report-types" : "report-types/",
+    "reports" : "reports/",
+    "roles" : "authorization/role",
+    "schedules" : "schedules/",
+    "tasks" : "tasks/",
+    "telemetry" : "telemetry/",
+    "users" : "idm/user/",
+    "version" : "version"
+}
+
 def main() :
-    get_choices = sorted(["agents", "plugins", "assets", "schedules", "policies",\
-        "replication", "granules", "classification-tags", "tasks", "jointokens",\
-        "telemetry", "roles", "version", "licenses",\
-        "reports", "report-types" ])
+
 
     parser = argparse.ArgumentParser(description='CloudPoint CLI help')
 
-#By default action="store" and type="string"
+    #By default action="store" and type="string"
     parser.add_argument("-g", "--get",help="Get information on some attribute.\
-    Allowed values are: {" +", ".join(get_choices) +"}", choices=get_choices, metavar='<option>')
+    Allowed values are: {" +", ".join(sorted(get_arg_mappings.keys())) +"}", choices=sorted(get_arg_mappings.keys()), metavar='<option>')
 
     parser.add_argument("-a", "--authenticate", action="store_true",\
         help="Login to CloudPoint ; Required for doing any other operation")
@@ -32,23 +50,12 @@ def main() :
 
 def interface(args) :
     x = api.Command()
-    no_slashes = ["granules", "version"]
     if args.get :
-        if args.get == "classification-tags":
-            endpoint = "classifications/tags"
-        elif args.get in no_slashes :
-            endpoint = args.get
-        elif args.get == "roles" :
-            endpoint = "authorization/role"
-        else :
-            endpoint=args.get+"/"
-        x.gets(endpoint)
+        x.gets(get_arg_mappings[args.get])
     elif  args.create :
         x.posts()
     elif  args.authenticate :
         x.authenticate()
-    else :
-        print("Invalid Option")
 
 if __name__ == "__main__" :
     main()
