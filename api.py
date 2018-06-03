4b6d33e916e43a4a67bcad65ea8e26cd9bef8acb
Ashwin Ramakrishnan <ashwinramki@gmail.com>
2018-06-03 19:46:34 -0400

    Linter corrections

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import requests 
 import json
 import getpass
+import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -10,71 +10,75 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 class Command():
 
-    def __init__(self) :
+    def __init__(self):
+
         self.endpoint = ''
         self.ip = '127.0.0.1'
         self.base_url = 'https://' + self.ip + ':/cloudpoint/api/v2'
         self.verify = False
+        self.token_header = ''
+        self.token_endpoint = ''
 
-        try :
-            with open("/root/.cldpt_token", "r") as file_handle :
+        try:
+            with open("/root/.cldpt_token", "r") as file_handle:
                 self.token = file_handle.readline()
-        except FileNotFoundError :
+        except FileNotFoundError:
             self.token = ''
 
         self.header = {'Content-Type': 'application/json',
-                    'Authorization': 'Bearer {0}'.format(self.token)}
+                       'Authorization': 'Bearer {0}'.format(self.token)}
 
-    def authenticate(self) :
+    def authenticate(self):
+
         self.token_header = {'Content-Type': 'application/json'}
-        self.token_endpoint= self.base_url + '/idm/login'
+        self.token_endpoint = self.base_url + '/idm/login'
         username = input("Username: ")
         passwd = getpass.getpass("Password: ")
         data = json.dumps({
             "email": username,
             "password": passwd})
-            
-        response = requests.post(self.token_endpoint, \
-                   verify=self.verify, headers=self.token_header, data=data)
-        if response.status_code == 200 :
-           self.token = ( (json.loads(\
-                        response.content.decode('utf-8')))["accessToken"] )
 
-           with open("/root/.cldpt_token", "w") as file_handle:
-               file_handle.write(self.token)
+        re = requests.post(self.token_endpoint, verify=self.verify,
+                           headers=self.token_header, data=data)
+        if re.status_code == 200:
+            self.token = (
+                (json.loads(re.content.decode('utf-8')))["accessToken"])
 
-        else :
-            print( (json.loads(response.content.decode('utf-8'))["errorMessage"]))
+            with open("/root/.cldpt_token", "w") as file_handle:
+                file_handle.write(self.token)
 
+        else:
+            print(
+                (json.loads(re.content.decode('utf-8'))["errorMessage"]))
 
     def gets(self, endpoint):
-        self.endpoint=endpoint
-        if not self.token :
+        self.endpoint = endpoint
+        if not self.token:
             print("\nPlease authenticate first !\n")
             exit()
 
         api_url = '{}/{}'.format(self.base_url, self.endpoint)
-        r = requests.get(api_url, headers=self.header, verify=self.verify)
+        re = requests.get(api_url, headers=self.header, verify=self.verify)
 
-        if r.status_code == 200 :
-            print (r.content.decode('utf-8'))
-        else :
-            print ('[!]ERROR : HTTP {0} calling [{1}]'.format(\
-                    r.status_code, api_url))
-            print("\n\nDETAILS : \n ", r.content.decode('utf-8'))
+        if re.status_code == 200:
+            print(re.content.decode('utf-8'))
+        else:
+            print('[!]ERROR : HTTP {0} calling [{1}]'.format
+                  (re.status_code, api_url))
+            print("\n\nDETAILS : \n ", re.content.decode('utf-8'))
 
     def posts(self):
-        if not self.token :
+        if not self.token:
             print("Please authenticate first !")
             exit()
 
         api_url = '{}/reports/'.format(self.base_url)
-        data="""{
+        data = """{
                "reportId": "pypy",
                "reportType": "snapshot",
                "columns": ["id", "name", "region", "ctime"]
         }"""
-        r = requests.post(api_url, data=data, verify=self.verify, headers=self.header)
-        val = json.loads((r.content.decode('utf-8')))
+        re = requests.post(api_url, data=data, verify=self.verify,
+                           headers=self.header)
+        val = json.loads((re.content.decode('utf-8')))
         print(val['msg'])
-
--- cldpt.py
+++ cldpt.py
@@ -8,24 +8,24 @@ import api
 import sys
 
 gets_dict = {
-    "ad-config" : "idm/config/ad",
-    "agents" : "agents/",
-    "assets" : "assets/",
-    "classification-tags" : "classifications/tags",
-    "granules" : "granules",
-    "jointokens" : "jointokens/",
-    "licenses" : "licenses/",
-    "plugins" : "plugins/",
-    "policies" : "policies/",
-    "replication" : "replication/",
-    "report-types" : "report-types/",
-    "reports" : "reports/",
-    "roles" : "authorization/role",
-    "schedules" : "schedules/",
-    "tasks" : "tasks/",
-    "telemetry" : "telemetry/",
-    "users" : "idm/user/",
-    "version" : "version"
+    "ad-config": "idm/config/ad",
+    "agents": "agents/",
+    "assets": "assets/",
+    "classification-tags": "classifications/tags",
+    "granules": "granules",
+    "jointokens": "jointokens/",
+    "licenses": "licenses/",
+    "plugins": "plugins/",
+    "policies": "policies/",
+    "replication": "replication/",
+    "report-types": "report-types/",
+    "reports": "reports/",
+    "roles": "authorization/role",
+    "schedules": "schedules/",
+    "tasks": "tasks/",
+    "telemetry": "telemetry/",
+    "users": "idm/user/",
+    "version": "version"
 }
 
 EXIT_1 = "\nERROR : Argument 'snapshots' requires -i flag for 'ASSET_ID'\n\
@@ -40,117 +40,122 @@ EXIT_3 = "\nERROR : Unknown option passed\n"
 EXIT_4 = "\nERROR : You need to provide an argument to SHOW\n\
 Expected Command Format : cldpt show assets ; cldpt show reports\n"
 
-def create_parser() :
 
+def create_parser():
 
-    parser_main = argparse.ArgumentParser(epilog="""\n \nFor help information \
-related to each sub-command/positional argument, \nUse "-h" or "--help" \
-at the end of that sub-command \n \nExamples : "cldpt create -h", \
-"cldpt show assets -i <ASSET_ID> --help" \n """,\
-    formatter_class=argparse.RawTextHelpFormatter)
+    parser_main = \
+        argparse.ArgumentParser(epilog="""\n \nFor help information
+ related to each sub-command/positional argument, \nUse "-h" or "--help"
+ at the end of that sub-command \n \nExamples : "cldpt create -h",
+ "cldpt show assets -i <ASSET_ID> --help" \n """,
+                                formatter_class=argparse.RawTextHelpFormatter)
     subparser_main = parser_main.add_subparsers(dest='command')
 
     parser_s = subparser_main.add_parser("show", help="show operations")
     subparser_s = parser_s.add_subparsers(dest="sub_command")
 
     parser_s_r = subparser_s.add_parser("reports")
-    parser_s_r.add_argument("-i", "--id", dest="report_id",\
-    help="Get information on a specific report ID")
-
+    parser_s_r.add_argument("-i", "--id", dest="report_id",
+        help="Get information on a specific report ID")
     parser_s_a = subparser_s.add_parser("assets")
-    parser_s_a.add_argument("-i", "--id", dest="asset_id",\
-    help="Get information on a specific asset ID")
+    parser_s_a.add_argument("-i", "--id", dest="asset_id",
+        help="Get information on a specific asset ID")
 
     subparser_s_a = parser_s_a.add_subparsers(dest="asset_command")
-    parser_s_a_s = subparser_s_a.add_parser ("snapshots")
-    parser_s_a_s.add_argument("-i","--id", dest="snap_id",\
-    help="Get information on a specific snapshot ID")
+    parser_s_a_s = subparser_s_a.add_parser("snapshots")
+    parser_s_a_s.add_argument("-i", "--id", dest="snap_id",
+        help="Get information on a snapshot ID")
 
     subparser_s_a_s = parser_s_a_s.add_subparsers(dest="snapshot_command")
     parser_s_a_s_g = subparser_s_a_s.add_parser("granules")
-    parser_s_a_s_g.add_argument("-i", "--id", dest="granule_id",\
-    help="Get information on a specific snapshot granule ID")
+    parser_s_a_s_g.add_argument("-i", "--id", dest="granule_id",
+        help="Get information on a specific snapshot granule ID")
 
-    parser_a = subparser_main.add_parser("authenticate", \
-    help="Login to CloudPoint ; Required for doing any other operation")
+    parser_a = subparser_main.add_parser("authenticate",
+        help="Login to CloudPoint ; Required for doing any operation")
 
-    parser_c = subparser_main.add_parser("create",\
-    help="Create any information within CloudPoint")
+    parser_c = subparser_main.add_parser("create",
+        help="Create any information within CloudPoint")
 
     return parser_main
 
-def interface(args) :
 
-    def check_attr(args, attr) :
-        try :
-            if hasattr(args, attr) :
-                if getattr(args, attr) :
+def interface(args):
+
+    def check_attr(args, attr):
+        try:
+            if hasattr(args, attr):
+                if getattr(args, attr):
                     return True
         except NameError:
             return False
-        else :
+        else:
             return False
 
     method_dict = {
-        "show" : "gets",
-        "create" : "posts",
-        "authenticate" : "authenticate"
+        "show": "gets",
+        "create": "posts",
+        "authenticate": "authenticate"
     }
 
     endpoint = []
-    
-    if args.command == "show" :
-        if args.sub_command == "assets" :
+
+    if args.command == "show":
+        if args.sub_command == "assets":
             endpoint.append(gets_dict[args.sub_command])
             if (check_attr(args, 'asset_id')):
                 endpoint.append(args.asset_id)
-            if (check_attr(args, 'asset_command') ) and (args.asset_command == "snapshots") :
-                if (check_attr(args, 'asset_id')) :
+            if (check_attr(args, 'asset_command')) and\
+               (args.asset_command == "snapshots"):
+                if (check_attr(args, 'asset_id')):
                     endpoint.append(args.asset_command)
-                    if (check_attr(args, 'snap_id')) :
+                    if (check_attr(args, 'snap_id')):
                         endpoint.append(args.snap_id)
-                else :
+                else:
                     print(EXIT_1)
                     sys.exit(2)
-            if (check_attr(args, 'snapshot_command') ) and (args.snapshot_command == "granules") :
-                if (check_attr(args, 'asset_id')) and (check_attr(args, 'snap_id')) :
+            if (check_attr(args, 'snapshot_command')) and\
+               (args.snapshot_command == "granules"):
+                if (check_attr(args, 'asset_id')) and\
+                   (check_attr(args, 'snap_id')):
                     endpoint.append(args.snapshot_command + '/')
-                    if (check_attr(args, 'granule_id')) :
+                    if (check_attr(args, 'granule_id')):
                         endpoint.append(args.granule_id)
-                else :
+                else:
                     print(EXIT_2)
                     sys.exit(3)
 
-        elif args.sub_command == "reports" :
+        elif args.sub_command == "reports":
             endpoint.append(gets_dict[args.sub_command])
-            if (check_attr(args, 'report_id')) :
+            if (check_attr(args, 'report_id')):
                 endpoint.append(args.report_id)
-        else :
+        else:
             print(EXIT_4)
             sys.exit(100)
 
-        getattr(api.Command(), method_dict[args.command]) ('/'.join(endpoint))
+        getattr(api.Command(), method_dict[args.command])('/'.join(endpoint))
 
-    elif args.command == "authenticate" :
-        getattr(api.Command(), method_dict[args.command]) ()
+    elif args.command == "authenticate":
+        getattr(api.Command(), method_dict[args.command])()
         sys.exit(4)
 
-    elif args.command == "create" :
-        getattr(api.Command(), method_dict[args.command]) ()
-    else :
+    elif args.command == "create":
+        getattr(api.Command(), method_dict[args.command])()
+    else:
         print(EXIT_3)
         sys.exit(5)
-        
-def main() :
+
+
+def main():
     parser = create_parser()
     argcomplete.autocomplete(parser)
     args = parser.parse_args(sys.argv[1:])
-    if len(sys.argv) == 1 :
+    if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(-1)
-    else :
+    else:
         interface(args)
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
