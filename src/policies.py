f57c6fbe47feddd58d757bb164be84e0e36d52ec
Ashwin Ramakrishnan <ashwinramki@gmail.com>
2018-08-11 23:59:15 -0400

    Finally added policy creation !!

--- src/policies.py
+++ src/policies.py
@@ -16,11 +16,16 @@ def entry_point(args):
         output = asset(args, endpoint)
 
     elif args.policies_command == 'create':
-        create()
+        endpoint.append('/policies/')
+        data = create()
+        print("endpoint is {}\nDATA IS {}".format(endpoint, data))
+        output = getattr(api.Command(), 'posts')('/'.join(endpoint), data)
 
     elif args.policies_command == 'delete':
+        endpoint.append('/policies/')
         delete(args, endpoint)
         output = getattr(api.Command(), 'deletes')('/'.join(endpoint))
+        print("Delete policy {}\n".format(args.policy_id))
 
     elif args.policies_command == 'show':
         endpoint.append('/policies/')
@@ -33,64 +38,6 @@ def entry_point(args):
         sys.exit(1)
 
     return output
-
-
-def show(args, endpoint):
-
-    if api.check_attr(args, 'policy_id'):
-        endpoint.append(args.policy_id)
-
-
-def create():
-
-    """
-    name, appConsist, tag, snapTypePref, hour = None, None, None, None, None
-    while True:
-        name = input("Policy Name : ")
-        if (len(name) > 32) or (len(name) < 2):
-            logger_c.error(
-                "Policy Name should be between 2 and 32 characters\n")
-        else:
-            break
-    while True:
-        appConsist = (input(
-            "Application Consistent ['yes' or 'no'] : ")).lower()
-        if appConsist not in ['yes', 'no']:
-            logger_c.error("\nValid options are 'yes' or 'no'\n")
-        else:
-            if appConsist == 'yes':
-                appConsist = True
-                break
-            else:
-                appConsist = False
-                break
-    tag = input("Description of Policy : ")
-    while True:
-        snapTypePref = (input("Snapshot type ['cow', 'clone'] : ")).lower()
-        if snapTypePref not in ['cow', 'clone']:
-            logger_c.error("\nValid options are 'cow' or 'clone'\n")
-        else:
-            break
-
-    logger_c.info(
-        "Schedule frequency options, Leave Blank if not applicable :\n")
-    minute = input("Minute (0-59): ") or "0"
-    hour = input("Hour (0-23): ") or "0"
-    month = input("Month : ") or "*"
-    wday = input("Day of the week : ") or "*"
-    mday = input("Date of the month: ") or "*"
-
-    schedule = {
-        "minute": minute,
-        "hour": hour,
-        "month": month,
-        "wday": wday,
-        "mday": mday
-    }
-    """
-
-    logger_c.error("Not implemented")
-    sys.exit(1)
 
 
 def asset(args, endpoint):
@@ -120,11 +67,187 @@ def asset(args, endpoint):
 
 def delete(args, endpoint):
 
-    endpoint.append('/policies/')
     endpoint.append(args.policy_id)
+
+
+def create():
+
+    name = None
+    while True:
+        name = input("Policy Name : ")
+        if (len(name) > 32) or (len(name) < 2):
+            logger_c.error(
+                "Policy Name should be between 2 and 32 characters\n")
+        else:
+            break
+
+    appConsist = True
+    while True:
+        appConsist = (input(
+            "Application Consistent ['y' or 'n'] (y) : ")).lower() or 'y'
+        if appConsist not in ['y', 'n']:
+            logger_c.error("Valid options are 'y' or 'n'\n")
+        else:
+            if appConsist == 'y':
+                appConsist = True
+                break
+            else:
+                appConsist = False
+                break
+
+    tag = input("Description of Policy : ") or ""
+
+    prot_level = None
+    prot_levels = ['disk', 'host', 'application']
+
+    while True:
+        logger_c.info("Specify Storage/Protection Type. Valid values are %s",
+                      prot_levels)
+        prot_level = input("Protection Type : ")
+        if prot_level in prot_levels:
+            break
+        else:
+            logger_c.error("Invalid Protection Type '%s'\n", prot_level)
+
+    replica = False
+    while True:
+        replica = (
+            input("Replicate Snapshot ['y' or 'n'] (n) : ")).lower() or 'n'
+        if replica == 'y':
+            replica = True
+            break
+        elif replica == 'n':
+            replica = False
+            break
+        else:
+            logger_c.error("Invalid Value. Valid options are 'y' OR 'n'\n")
+
+    sched_types = ['minutes', 'hourly', 'daily', 'weekly', 'monthly']
+    sched_type = None
+    while True:
+        logger_c.info("Specify schedule type, valid options are %s", sched_types)
+        sched_type = input("Schedule Type : ")
+        if sched_type not in sched_types:
+            logger_c.info("'%s' Not a valid schedule type\n", sched_type)
+        else:
+            break
+
+    minute = "0"
+    hour = "0"
+    month =  "*"
+    wday = "*"
+    mday = "*"
+    valid_wkdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday',
+                    'saturday', 'sunday']
+
+    if sched_type == 'minutes':
+        while True:
+            logger_c.info("Snapshot every _ minutes? Valid range:%s", '[0-59]')
+            minute = input("minutes : ")
+            if minute in str(list(range(60))):
+                break
+            else:
+                logger_c.error("Invalid Value '%s'\n", minute)
+
+    elif sched_type == 'hourly':
+        while True:
+            logger_c.info("Snapshot every _ hours? Valid range:%s", '[0-23]')
+            hour = input("hour : ")
+            if hour in str(list(range(24))):
+                break
+            else:
+                logger_c.error("Invalid Value '%s'\n", hour)
+
+    elif sched_type == 'daily':
+        while True:
+            logger_c.info("Snapshot every day at _ ? Valid range:%s", '[0-23]')
+            hour = input("hour (0): ") or "0"
+            if hour in str(list(range(24))):
+                break
+            else:
+                logger_c.error("Invalid Value '%s'\n", hour)
+
+    elif sched_type == 'weekly':
+        while True:
+            logger_c.info("Snapshot every week on _ ? Valid values:%s",
+                          valid_wkdays)
+            wday = input("Day of the week : ")
+            if wday in valid_wkdays:
+                while True:
+                    logger_c.info("Snapshot at _ ? Valid range:%s", '[0-23]')
+                    hour = input("hour (0): ") or "0"
+                    if hour in str(list(range(24))):
+                        break
+                    else:
+                        logger_c.error("Invalid Value '%s'\n", hour)
+                break
+            else:
+                logger_c.error("Invalid Value '%s'\n", wday)
+
+    else:
+        while True:
+            logger_c.info("Snapshot every month on _ ? Valid values:%s",
+                          '[1-31]')
+            mday = input("Date : ")
+            if mday in str(list(range(1, 32))):
+                while True:
+                    logger_c.info("Snapshot at _ ? Valid range:%s", '[0-23]')
+                    hour = input("hour (0): ") or "0"
+                    if hour in str(list(range(24))):
+                        break
+                    else:
+                        logger_c.error("Invalid Value '%s'\n", hour)
+                break
+            else:
+                logger_c.error("Invalid Value '%s'\n", mday)
+
+
+    schedule = {
+        "minute": minute,
+        "hour": hour,
+        "month": month,
+        "wday": wday,
+        "mday": mday
+    }
+
+    ret_unit_types = ['snapshots', 'days', 'weeks', 'months', 'years']
+    ret_unit_type = None
+    while True:
+        logger_c.info("Enter the retention unit type. Valid values are %s",
+                      ret_unit_types)
+        ret_unit_type = input("Retention unit type : ")
+        if ret_unit_type in ret_unit_types:
+            break
+
+    ret_val = int(input("Retention Value : "))
+
+    retention = {
+        "unit": ret_unit_type,
+        "value": ret_val
+    }
+
+    data = {
+        "name": name,
+        "tag": tag,
+        "protectionLevel": prot_level,
+        "replicate": replica,
+        "appConsist": appConsist,
+        "retention": retention,
+        "schedule": schedule
+    }
+
+    return data
 
 
 def pretty_print(data):
     # This function has to be tailor suited for each command's output
     # Since all commands don't have a standard output format
     print(data)
+
+
+def show(args, endpoint):
+
+    if api.check_attr(args, 'policy_id'):
+        endpoint.append(args.policy_id)
+
+
